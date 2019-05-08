3f56c6ab8c1a1d57f2d46124decac99d59569481
BJ <bj@zo0d.hitronhub.home>
2019-05-07 22:09:58 -0400

    init

---
+++ petitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  petitions
+//
+//  Created by BJ on 2019-05-07.
+//  Copyright © 2019 BJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
